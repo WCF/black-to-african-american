19976555a9cee66e7c79dfaac16876ec84e3ea98
Brian Lai <brian@rideco.com>
2017-06-02 10:23:48 -0400

    add more content warnings

--- Source/content_script.js
+++ Source/content_script.js
@@ -49,6 +49,18 @@
         }
     }
 
+
+    function matchCase(sub) {
+        return function (match) {
+            var p = match.charAt(0);
+            if (p >= 65 && p < 65 + 26) {
+                return sub[0].toUpperCase() + sub.substring(1);
+            } else {
+                return sub[0].toLowerCase() + sub.substring(1);
+            }
+        };
+    }
+
     function handleText(textNode) {
         var v = textNode.nodeValue;
 
@@ -60,23 +72,48 @@
         // https://imgur.com/sEbuvTs
         v = v.replace(/\bWhite\b/ig, "Caucasian American");
 
+        // http://www.voont.com/the-alphabet-of-swearing
+        v = v.replace(/ass/ig, matchCase("butt"));  // We need the clbuttic feature back
+        v = v.replace(/\bbrothers?\b/ig, "B word");
+        v = v.replace(/\bcunts?\b/ig, "C word");
+        v = v.replace(/\bdick/ig, "Richard");
+        // e
+        v = v.replace(/\bfuck/ig, "F word");
+        v = v.replace(/\bgay/ig, "G word");
+        // h
+        // i
+        // j
+        // k
+        // l
+
+        // https://imgur.com/JEBabGs
+        v = v.replace(/\bmidgets?(\b)?/ig, "M word");
         // http://www.telegraph.co.uk/culture/film/film-news/9777404/Django-Unchained-star-Samuel-L-Jackson-challenges-TV-reporter-to-say-N-word.html
         // Think of the children!
         v = v.replace(/\bnigg(a|er)s?(\b)?/ig, "N word");
-        // https://imgur.com/JEBabGs
-        v = v.replace(/\bmidgets?(\b)?/ig, "M word");
+
+        v = v.replace(/\boppression\b/ig, "O word");
+        v = v.replace(/\bpee\b/ig, "P word");
+        v = v.replace(/\bpenis\b/ig, "P word");
+        v = v.replace(/\bqueer\b/ig, "Q word");
+        // r
+        v = v.replace(/\bshit\b/ig, "S word");
+        v = v.replace(/\btits\b/ig, "T word");
+        // u
+        // v
+        v = v.replace(/\bvagina\b/ig, "V word");
+        // w
+        // x
+        // y
+        // z
 
         // https://imgur.com/PAjutIv
         if (Math.random() > 0.5) {
-            v = v.replace(/\bmiddle[-\s]aged man/g, "pedophile");
-            v = v.replace(/\bMiddle[-\s]aged man/g, "Pedophile");
-            v = v.replace(/\bmiddle[-\s]aged men/g, "pedophiles");
-            v = v.replace(/\bMiddle[-\s]aged men/g, "Pedophiles");
+            v = v.replace(/\bmiddle[-\s]aged man/ig, matchCase("pedophile"));
+            v = v.replace(/\bMiddle[-\s]aged men/ig, matchCase("Pedophiles"));
         } else {  // Really hard to tell which one
-            v = v.replace(/\bmiddle[-\s]aged man/g, "rapist");
-            v = v.replace(/\bMiddle[-\s]aged man/g, "Rapist");
-            v = v.replace(/\bmiddle[-\s]aged men/g, "rapists");
-            v = v.replace(/\bMiddle[-\s]aged men/g, "Rapists");
+            v = v.replace(/\bmiddle[-\s]aged man/ig, matchCase("rapist"));
+            v = v.replace(/\bmiddle[-\s]aged men/ig, matchCase("rapists"));
         }
 
         // Bonus Tumblr edition
@@ -91,8 +128,7 @@
         v = v.replace(/\bWomen/g, "Womyn");
         v = v.replace(/\bwomen/g, "womyn");
         // https://i.redd.it/n86l1nydhb8y.png
-        v = v.replace(/\bdude/g, "person");
-        v = v.replace(/\bDude/g, "Person");
+        v = v.replace(/\bdude/ig, matchCase("person"));
 
         if (Math.random() > 0.5) {
             v = v.replace(/\bstraight\b/g, "cisgender");
@@ -110,13 +146,15 @@
         // http://www.cbsnews.com/news/examiner-recommends-school-board-uphold-pop-tart-suspension/
         v = v.replace(/\bgun(\b)?/ig, "pop tart");
         // https://www.youtube.com/watch?v=aT-MhzcP4U8&feature=share&app=desktop
-        v = v.replace(/\bmexican(\b)?/g, "undocumented immigrant");
-        v = v.replace(/\bMexican(\b)?/g, "Undocumented immigrant");
+        v = v.replace(/\bmexican(\b)?/ig, matchCase("undocumented immigrant"));
         // https://thebodyisnotanapology.com/magazine/11-offensive-phrases-you-didnt-realize-are-fat-shaming/
+
         v = v.replace(/\bfat shaming/g, "beauty contest");
         v = v.replace(/\bFat [Ss]haming/g, "Beauty contest");
         v = v.replace(/\bfat/g, "beautiful");
         v = v.replace(/\bFat/g, "Beautiful");
+        v = v.replace(/\boleophobic\b/ig, matchCase("fatphobic"));
+
         // https://imgur.com/WbsO2X8
         v = v.replace(/\bmirrors/g, "instruments of oppression");
         v = v.replace(/\bMirrors/g, "Instruments of oppression");
@@ -142,14 +180,19 @@
         v = v.replace(/\bSex\b/g, "Rape");
         v = v.replace(/\bsex\b/g, "rape");
         // https://www.reddit.com/r/TumblrInAction/comments/500bs1/trigger_warning/d70gy6m/
-        v = v.replace(/\btrigger\b/g, "the T word");
-        v = v.replace(/\bTrigger\b/g, "The T word");
+        v = v.replace(/\btrigger\b/ig, matchCase("the T word"));
         // https://i.redd.it/h87cf9we9lyx.jpg
-        v = v.replace(/\bright[-\s]handed\b/g, "privileged");
-        v = v.replace(/\bRight[-\s]handed\b/g, "Privileged");
+        v = v.replace(/\bright[-\s]handed\b/ig, matchCase("privileged"));
         // https://i.redd.it/4vl78mu7r0ly.jpg
         v = v.replace(/\bporn\b/g, "hate crime against womyn");
         v = v.replace(/\bPorn\b/g, "Hate crime against womyn");
+        // https://heatst.com/tech/youtube-ceo-deems-interrupting-her-a-sexist-microaggression/
+        v = v.replace(/\binterruptions\b/g, "sexist microaggression");
+        // http://www.dailymail.co.uk/travel/travel_news/article-4522350/Hilarious-photographs-women-FEMSPREADING.html
+        v = v.replace(/\bfemspreading\b/ig, "MANSPREADING");
+        // https://townhall.com/tipsheet/mattvespa/2017/05/20/in-seattle-police-can-no-longer-report-suspects-they-have-to-say-community-members-n2329542?
+        v = v.replace(/\bsuspect/ig, matchCase("community member"));  // even if it is a verb
+        v = v.replace(/\bcitizen/ig, matchCase("community member"));  // even if it is the watch brand
 
         textNode.nodeValue = v;
     }
